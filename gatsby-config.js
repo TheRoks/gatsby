3152cc3263d6bc9632690522255ad28e4b9d89bb
Stefan Roks <stefanroks@gmail.com>
2019-10-05 22:04:01 +0200

    site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,19 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
-    description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+    title: "TheRoks",
+    author: "Stefan Roks",
+    description: "A collection of my thoughts and writings.",
+    siteUrl: "https://theroks.nl",
+    social: [
+      {
+        name: "twitter",
+        url: "https://twitter.com/theroks",
+      },
+      {
+        name: "github",
+        url: "https://github.com/theroks",
+      },
+    ],
   },
   plugins: [
     'gatsby-plugin-react-helmet',
